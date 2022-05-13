ca85653617ffead6cc011b7da71785b95ac2fe3c
supremedacos <nightowlcasino@protonmail.com>
2022-05-13 19:44:16 -0400

    more indents to fix

--- models.go
+++ models.go
@@ -1,20 +1,20 @@
 package main
 
 type EthBlockNumber struct {
-	JsonRPC string `json:"jsonrpc"`
-	Id      int    `json:"id"`
-	Result  string `json:"result"`
+  JsonRPC string `json:"jsonrpc"`
+  Id      int    `json:"id"`
+  Result  string `json:"result"`
 }
 
 type Block struct {
-	Hash       string `json:"hash"`
-	ParentHash string `json:"parentHash"`
-	Size       string `json:"size"`
-	Timestamp  string `json:"timestamp"`
+  Hash       string `json:"hash"`
+  ParentHash string `json:"parentHash"`
+  Size       string `json:"size"`
+  Timestamp  string `json:"timestamp"`
 }
 
 type EthBlock struct {
-	JsonRPC string `json:"jsonrpc"`
-	Id      int    `json:"id"`
-	Block   Block  `json:"result"`
+  JsonRPC string `json:"jsonrpc"`
+  Id      int    `json:"id"`
+  Block   Block  `json:"result"`
 }
